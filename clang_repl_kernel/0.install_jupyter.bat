cf0a04e0f90a774cfda23b0d550128b2320cd772
yoon <ormastes@gmail.com>
2023-09-27 09:21:38 +0900

    Linux shell script added.

--- clang_repl_kernel/0.install_jupyter.bat
+++ clang_repl_kernel/0.install_jupyter.bat
@@ -1,3 +1,4 @@
 python -m pip install notebook
 python -m pip install twine
 python -m pip install build
+rem reopen shell to affect install
---
+++ clang_repl_kernel/0.install_jupyter.sh
@@ -0,0 +1,5 @@
+python3 -m pip install notebook
+python3 -m pip install twine
+python3 -m pip install build
+sudo apt-get install python3-venv
+# reopen shell to affect install
